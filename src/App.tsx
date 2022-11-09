000f8a4dfa0c8227defaa70670707f0e5d248a24
kikilove5me <77271656+kikilove5me@users.noreply.github.com>
2022-11-09 11:52:57 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ const useInput = (initValue: any) => {
   const [value, setValue] = React.useState(initValue);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+  };
+
+  const clear = () => setValue('');
+  return {
+    bind: { value, onChange },
+    value,
+    clear
   }
-  return { value, onChange }
 }
 
 function App() {
@@ -14,7 +20,8 @@ function App() {
   return (
     <div className={"container pt-3"}>
       <h1>{input.value}</h1>
-      <input type="text" placeholder="write something" {...input} />
+      <input type="text" placeholder="write something" {...input.bind} />
+      <button className="btn btn-warning" onClick={() => input.clear()}>clear</button>
     </div>
 
   );
